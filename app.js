bd3ba580cc8aa3378d942fe4c7dba0a63af336e5
cody <codywengland@gmail.com>
2024-04-14 15:30:57 -0500

    made

--- app.js
+++ app.js
@@ -4,192 +4,263 @@ const taskItemBtn = document.getElementById("task-container");
 const inputContainer = document.getElementById("input-container");
 const deleteAll = document.querySelector(".delete-task");
 const mainBody = document.getElementById("main-content-container");
-let input = document.getElementById("input");
+const input = document.getElementById("input");
 const filter = document.getElementById("filters-container");
 const itemFilterBtn = document.querySelectorAll(".item");
 
 function addTask(e) {
-  let date = document.getElementById("date-picker").value;
+	let date = document.getElementById("date-picker").value;
 
-  //getting value from user input
-  const userInput = input.value;
+	//getting value from user input
+	const userInput = input.value;
 
-  //creating all the html elemets for task
-  const taskItemContainer = document.createElement("div");
-  const leftTaskContainer = document.createElement("div");
-  const trashCan = document.createElement("img");
-  const starTask = document.createElement("img");
-  const userTask = document.createElement("p");
-  let addDate = document.createElement("p");
-  const rightTaskContainer = document.createElement("div");
+	//creating all the html elemets for task
+	const taskItemContainer = document.createElement("div");
+	const leftTaskContainer = document.createElement("div");
+	const trashCan = document.createElement("img");
+	const starTask = document.createElement("img");
+	const userTask = document.createElement("p");
+	let addDate = document.createElement("p");
+	const rightTaskContainer = document.createElement("div");
 
-  //adding the classes to the elements
-  taskItemContainer.classList.add("task-item");
-  leftTaskContainer.classList.add("left-side-task");
-  trashCan.classList.add("task-delete-single", "item-pic");
-  starTask.classList.add("starred-select", "item-pic");
-  rightTaskContainer.classList.add("right-task-list");
-  userTask.classList.add("user-task");
+	//adding the classes to the elements
+	taskItemContainer.classList.add("task-item", "all");
+	leftTaskContainer.classList.add("left-side-task");
+	trashCan.classList.add("task-delete-single", "item-pic");
+	starTask.classList.add("starred-select", "item-pic");
+	rightTaskContainer.classList.add("right-task-list");
+	userTask.classList.add("user-task");
 
-  //adding transistion 
-   setTimeout(() => {
-        taskItemContainer.classList.add('slide-in');
-    }, 10);
+	//adding transistion
+	setTimeout(() => {
+		taskItemContainer.classList.add("slide-in");
+	}, 10);
 
-  //adding the img soruce to the elements
-  trashCan.src = "img/trash-can.png";
-  starTask.src = "img/star-unfilled.png";
+	//adding the img soruce to the elements
+	trashCan.src = "img/trash-can.png";
+	starTask.src = "img/star-unfilled.png";
 
-  //adding date
-  const slicedDate = date.slice(-5);
-  addDate.innerHTML = `${slicedDate}`;
+	//adding date
+	const slicedDate = date.slice(-5);
+	addDate.innerHTML = `${slicedDate}`;
 
-  //adding all the js made elements to the html
-  userTask.innerHTML = userInput;
-  taskContainer.appendChild(taskItemContainer);
-  taskItemContainer.appendChild(leftTaskContainer);
-  taskItemContainer.appendChild(rightTaskContainer);
-  leftTaskContainer.appendChild(trashCan);
-  leftTaskContainer.appendChild(userTask);
-  rightTaskContainer.appendChild(starTask);
-  rightTaskContainer.appendChild(addDate);
+	//adding all the js made elements to the html
+	userTask.innerHTML = userInput;
+	taskContainer.appendChild(taskItemContainer);
+	taskItemContainer.appendChild(leftTaskContainer);
+	taskItemContainer.appendChild(rightTaskContainer);
+	leftTaskContainer.appendChild(trashCan);
+	leftTaskContainer.appendChild(userTask);
+	rightTaskContainer.appendChild(starTask);
+	rightTaskContainer.appendChild(addDate);
 
-  //erasing the input field
-  input.value = "";
+	//erasing the input field
+	input.value = "";
 }
 
 function taskItems(e) {
-  //pulling task item
-  const taskItemContainer = document.querySelector(".task-item");
+	//pulling task item
+	const taskItemContainer = document.querySelector(".task-item");
 
-  const starFilled = document.querySelector(".starred-select");
-  //pulling elemt that is click
-  const task = e.target;
-  const parentDiv = task.parentNode.parentNode;
-  if (task.classList.contains("task-delete-single")) {
+	//pulling elemt that is click
+	const task = e.target;
+	const parentDiv = task.parentNode.parentNode;
+	if (task.classList.contains("task-delete-single")) {
+		parentDiv.classList.add("slide-out");
 
-    parentDiv.classList.add("slide-out");
-
-    setTimeout(() => {
-      parentDiv.remove(taskItemContainer);
-  }, 300);
-  } else if (
-    task.classList.contains("starred-select") &&
-    task.classList.contains("star-selected")
-  ) {
-    task.src = "img/star-unfilled.png";
-    task.classList.remove("star-selected");
-  } else if (task.classList.contains("starred-select")) {
-    task.src = "img/star-filled.png";
-    task.classList.add("star-selected");
-    taskItemContainer.classList.add("starred");
-  }
+		setTimeout(() => {
+			parentDiv.remove(taskItemContainer);
+		}, 300);
+	} else if (
+		task.classList.contains("starred-select") &&
+		task.classList.contains("star-selected")
+	) {
+		task.src = "img/star-unfilled.png";
+		task.classList.remove("star-selected");
+	} else if (task.classList.contains("starred-select")) {
+		parentDiv.classList.remove("all");
+		task.src = "img/star-filled.png";
+		task.classList.add("star-selected", "starred");
+		parentDiv.classList.add("starred");
+	}
 }
 
 function checkUser(e) {
-  //getting the users input and triming extra spaces
-  const userInput = document.getElementById("input").value.trim();
+	//getting the users input and triming extra spaces
+	const userInput = document.getElementById("input").value.trim();
 
-  //formats all elements into an array to check to see if pop up is already present
-  let hasChildWithClass = Array.from(inputContainer.children).some(function (
-    promt
-  ) {
-    return promt.classList.contains("promt");
-  });
+	//formats all elements into an array to check to see if pop up is already present
+	let hasChildWithClass = Array.from(inputContainer.children).some(function (
+		promt
+	) {
+		return promt.classList.contains("promt");
+	});
 
-  //checks to see if users input is empty when submitted
-  if (userInput === "") {
-    const promtInput = document.createElement("p");
+	//checks to see if users input is empty when submitted
+	if (userInput === "") {
+		const promtInput = document.createElement("p");
 
-    if (hasChildWithClass) {
-      return;
-    } else {
-      //sets prompt if user input is empty
-      promtInput.classList.add("promt");
-      promtInput.innerHTML = "*Please Enter A Task";
-      inputContainer.appendChild(promtInput);
-    }
-  } else if (userInput !== "") {
-    if (hasChildWithClass) {
-      //if prompt is already present and input has informations subbmited it removes the prompt and runs addtask()
-      const promtRemove = document.querySelector(".promt");
-      promtRemove.remove();
-      addTask();
-    } else {
-      addTask();
-    }
-  }
+		if (hasChildWithClass) {
+			return;
+		} else {
+			//sets prompt if user input is empty
+			promtInput.classList.add("promt");
+			promtInput.innerHTML = "*Please Enter A Task";
+			inputContainer.appendChild(promtInput);
+		}
+	} else if (userInput !== "") {
+		if (hasChildWithClass) {
+			//if prompt is already present and input has informations subbmited it removes the prompt and runs addtask()
+			const promtRemove = document.querySelector(".promt");
+			promtRemove.remove();
+			addTask();
+		} else {
+			addTask();
+		}
+	}
 }
 
 function deleteAllTask(e) {
-  //creates a overlay and modal elements when deleting all task
-  const overlayContainer = document.createElement("div");
-  const modalContainer = document.createElement("div");
-  const message = document.createElement("p");
-  const btnConfirm = document.createElement("a");
-  const btnCancel = document.createElement("a");
+	//creates a overlay and modal elements when deleting all task
+	const overlayContainer = document.createElement("div");
+	const modalContainer = document.createElement("div");
+	const message = document.createElement("p");
+	const btnConfirm = document.createElement("a");
+	const btnCancel = document.createElement("a");
 
-  //adding classes to modal and overlay elements
-  overlayContainer.classList.add("overlay");
-  modalContainer.classList.add("modal");
-  btnConfirm.classList.add("btn", "confirm-delete");
-  btnCancel.classList.add("btn", "negative-delete");
+	//adding classes to modal and overlay elements
+	overlayContainer.classList.add("overlay");
+	modalContainer.classList.add("modal");
+	btnConfirm.classList.add("btn", "confirm-delete");
+	btnCancel.classList.add("btn", "negative-delete");
 
-  //add elements to body
-  mainBody.appendChild(overlayContainer);
-  mainBody.appendChild(modalContainer);
+	//add elements to body
+	mainBody.appendChild(overlayContainer);
+	mainBody.appendChild(modalContainer);
 
-  //adds text to elements
-  message.innerHTML = "Do you want to delete all task?";
-  btnConfirm.innerHTML = "Confirm";
-  btnCancel.innerHTML = "Cancel";
+	//adds text to elements
+	message.innerHTML = "Do you want to delete all task?";
+	btnConfirm.innerHTML = "Confirm";
+	btnCancel.innerHTML = "Cancel";
 
-  //add elements to body
-  modalContainer.appendChild(message);
-  modalContainer.appendChild(btnConfirm);
-  modalContainer.appendChild(btnCancel);
+	//add elements to body
+	modalContainer.appendChild(message);
+	modalContainer.appendChild(btnConfirm);
+	modalContainer.appendChild(btnCancel);
 
-  //listens for the confirm or cancel button for overlay
-  btnConfirm.addEventListener("click", function () {
-    while (taskContainer.firstChild) {
-      taskContainer.removeChild(taskContainer.firstChild);
-    }
+	//listens for the confirm or cancel button for overlay
+	btnConfirm.addEventListener("click", function () {
+		while (taskContainer.firstChild) {
+			taskContainer.removeChild(taskContainer.firstChild);
+		}
 
-    overlayContainer.remove(overlayContainer);
-    modalContainer.remove(modalContainer);
-  });
+		overlayContainer.remove(overlayContainer);
+		modalContainer.remove(modalContainer);
+	});
 
-  btnCancel.addEventListener("click", function () {
-    overlayContainer.remove(overlayContainer);
-    modalContainer.remove(modalContainer);
-    return;
-  });
+	btnCancel.addEventListener("click", function () {
+		overlayContainer.remove(overlayContainer);
+		modalContainer.remove(modalContainer);
+		return;
+	});
 }
 
 function filterItems(e) {
- const filterTasks =  e.target;
+	const taskItem = document.querySelectorAll(".task-item");
 
- const filterBtns = Array.from(itemFilterBtn).filter(itemFilterBtn => itemFilterBtn.classList.contains("selected-items"))
+	const filterTasks = e.target;
 
- filterBtns.forEach(itemFilterBtn => {
-  itemFilterBtn.classList.remove("selected-items");
-});
+	const filterBtns = Array.from(itemFilterBtn).filter((itemFilterBtn) =>
+		itemFilterBtn.classList.contains("selected-items")
+	);
 
- if (filterTasks.classList.contains("item")) {
-  filterTasks.classList.add("selected-items");
- }
+	filterBtns.forEach((itemFilterBtn) => {
+		itemFilterBtn.classList.remove("selected-items");
+	});
 
-};
+	if (filterTasks.classList.contains("item")) {
+		filterTasks.classList.add("selected-items");
+	}
 
+	//FILTER
 
+	if (
+		filterTasks.classList.contains("star") ||
+		filterTasks.classList.contains("all") ||
+		filterTasks.classList.contains("today") ||
+		filterTasks.classList.contains("week")
+	) {
+		let parentDiv = document.getElementById("task-inventory-container");
+
+		let childDivsWithClassName = parentDiv.querySelectorAll(".task-item");
+
+		childDivsWithClassName.forEach(function (childDiv) {
+			if (filterTasks.classList.contains("all")) {
+				if (
+					childDiv.classList.contains("all") ||
+					childDiv.classList.contains("starred")
+				) {
+					childDiv.style.display = "flex";
+				}
+			}
+
+			if (filterTasks.classList.contains("star")) {
+				if (childDiv.classList.contains("all")) {
+					childDiv.style.display = "none";
+				}
+			}
+
+			if (filterTasks.classList.contains("today")) {
+				return;
+			}
+
+			if (filterTasks.classList.contains("week")) {
+				getEndOfWeek(endOfWeek);
+				getStartOfWeek(startOfWeek);
+
+				let startOfWeek = getStartOfWeek();
+				let endOfWeek = getEndOfWeek();
+
+				console.log(
+					"Start of the week:",
+					startOfWeek.toDateString().slice(7, 10)
+				);
+				console.log("End of the week:", endOfWeek.toDateString());
+				console.log("nice");
+			}
+		});
+	}
+}
+
+// date
+function getStartOfWeek() {
+	let today = new Date();
+	let dayOfWeek = today.getDay();
+	let startOfWeek = new Date(today);
+
+	let diff = today.getDate() - dayOfWeek + (dayOfWeek === 0 ? -6 : 1);
+
+	startOfWeek.setDate(diff);
+
+	return startOfWeek;
+}
+
+function getEndOfWeek() {
+	let startOfWeek = getStartOfWeek();
+	let endOfWeek = new Date(startOfWeek);
+
+	endOfWeek.setDate(startOfWeek.getDate() + 6);
+
+	return endOfWeek;
+}
 
 //event listeners
-filter.addEventListener("click", filterItems)
+filter.addEventListener("click", filterItems);
 taskAddBtn.addEventListener("click", checkUser);
 taskItemBtn.addEventListener("click", taskItems);
 deleteAll.addEventListener("click", deleteAllTask);
 input.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    checkUser();
-  }
+	if (event.key === "Enter") {
+		checkUser();
+	}
 });
